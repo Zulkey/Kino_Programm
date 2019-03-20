295e354efd0a50515874f34ef5f12a09b56ae572
soehnkeg <soehnkeg@gmail.com>
2019-03-20 13:39:55 +0100

    Projektdateien hinzufügen.

---
+++ Kino_Pruefungsvorbereitung/Program.cs
@@ -0,0 +1,158 @@
+﻿using CinemaSeatController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;    //Wird verwendet um das schreiben in Textdateien zu ermöglichen.
+
+namespace Kino_Pruefungsvorbereitung
+{
+    public class Program
+    {
+
+        
+        public static string csvUhrzeit;    //Uhrzeit, wird in der "SeatControllerUI.cs" verwendet. "Public static" wird verwendet, damit andere Klassen auf die Variable zugreifen können.
+        public static int csvSaal;          //Uhrzeit, wird in der "SeatControllerUI.cs" verwendet. "Public static" wird verwendet, damit andere Klassen auf die Variable zugreifen können.
+        public static string csvPlatz;      //Uhrzeit, wird in der "SeatControllerUI.cs" verwendet. "Public static" wird verwendet, damit andere Klassen auf die Variable zugreifen können.
+
+
+
+        static void Main(string[] args)
+        {
+            SeatControllerUI seatControllerUI;      //
+
+            //Deklaration und Initialisierung
+            int anzahlSaele = 5;                    //Variable für die Anzahl der Kinosäle
+            int eingabeSaal = 0;                    //Variable, in welcher die Eingabe für die Auswahl des Saales gespeichert wird.
+
+            string vorstellung1 = "17:00";          //Um "zahlen" im späteren Code zu vermeiden, eine Variable, die später als Vergleich für die Eingabe der Uhrzeit dient.
+            string vorstellung2 = "20:00";          //Um "zahlen" im späteren Code zu vermeiden, eine Variable, die später als Vergleich für die Eingabe der Uhrzeit dient.
+            char switchcase;                        //Variable die für die Auswahl zwischen "Reservierung ansehen" und "Reservierung vornehmen" verwendet wird.
+            string eingabeUhrzeit = null;           //In diesem String wird die Eingabe der Uhrzeit gespeichert.
+           
+
+
+            //Instanzbildung
+            int[] sal1 = { 10, 15 }; //Array mit Reihen und Plätzen für Saal 1.
+            int[] sal2 = { 20, 10 }; //Array mit Reihen und Plätzen für Saal 2.
+            int[] sal3 = { 15, 20 }; //Array mit Reihen und Plätzen für Saal 3.
+            int[] sal4 = { 15, 20 }; //Array mit Reihen und Plätzen für Saal 4.
+            int[] sal5 = { 20, 20 }; //Array mit Reihen und Plätzen für Saal 5.
+
+            //Ändern der Farbe der Console auf:
+            //Schwarzen Text und
+            //einen weißen Hintergrund.
+
+
+            //Eingabe "Auswahl"
+            Console.WriteLine("Willkomen im 'Complex'!");       //Begrüßungsnachricht
+            Console.Write("Wollen Sie einen Reservierung vornehmen, dann drücken Sie 'R'. \nWollen Sie eine bereits vorgenommene Reservierung ansehen? Dann drücken Sie 'S'."); //Nachricht für Auswahl, ob der Anwender seine Reservierung ansehen möchte, oder eine solche vornehmen möchte.
+            switchcase = Convert.ToChar(Console.ReadLine());    //Die Eingabe wird gespeichert.
+
+            Console.WriteLine();                                //Absatz
+            switchcase = Char.ToUpper(switchcase);              //Sollte die Eingabe einen kleinen Buchstaben enthalten, wird dieser hier zu einem Großbuchstaben.
+            switch (switchcase)                                 //Überprüfen des Wertes der Variable "switchcase".      
+            {
+                case 'R':                                                                                                           //Sollte die Variable den Wert "R" besitzen, wird folgendes gemacht: 
+                    {
+                        eingabeUhrzeitgoto:                                                                                         //"goto" der Uhrzeit, sollte die Uhrzeit nicht den Werten vorgegebenen Werten entsprechen, wird hierhin gesprungen.
+                        Console.Write("Um wie viel Uhr wollen Sie den Film schauen?\n17:00 für 17 Uhr bzw. 20:00: ");               //Nachricht uzr Auswahl der Uhrzeit
+                        eingabeUhrzeit = Convert.ToString(Console.ReadLine());                                                      //Eingabe wird in einem String gespeichert
+                        Console.WriteLine();                                                                                        //Absatz
+                        
+                        if (eingabeUhrzeit.Equals(vorstellung1))                                                                    //Sollte die Eingabe dem ganz oben festgelegten Wert entsprechen (wird mit "String.Equals überprüft
+                        {
+                            Console.WriteLine("Sie haben erfolgreich die Vorstellung um {0} Uhr reserviert. ",vorstellung1);        //Nachricht, dass erfolgreich reserviert wurde
+                            Sitzplatz.normalPrice = 10;                                                                             //Der Preis wird auf 10€ gesetzt
+                            Sitzplatz.premiumPrice = 8;                                                                             //Der Preis für die ersten 5 Reihen wird auf 8€ gesetzt
+                            Program.csvUhrzeit = (vorstellung1);                                                                    //Wert wird in einer Variable gespeichert welche später von einer anderen Klasse verwendet wird.
+                        } else if (eingabeUhrzeit.Equals(vorstellung2))                                                                 //Sollte dem nicht so sein, so wird geschaut ob der eingegebene Wert der zweiten Variable entspricht.
+                        {
+                            Console.WriteLine("Sie haben erfolgreich die Vorstellung um {0} Uhr reserviert. ", vorstellung2);       //sollte dem so sein, kommt eine Nachricht, das reserviert wurde.
+                            Sitzplatz.normalPrice = 15;                                                                             //Preis für normale Plätze wird auf 15€ gesetzt
+                            Sitzplatz.premiumPrice = 12;                                                                            //Preis für die ersten 5 Reihen wird auf 12€ gesetzt
+                            csvUhrzeit = vorstellung2;                                                                              //ganz oben festgelegte Variable bekommt den Wert der Uhrzeit
+                        }
+                        else if(eingabeUhrzeit == null)                                                                             //Überprüfen ob der eingegebene Wert "0" entspricht.
+                        {
+                            Console.WriteLine("Bitte gebe alle Daten korrekt ein!");                                                  //Aufforderung die Daten korrekt einzugeben
+                            goto eingabeUhrzeitgoto;                                                                                  //Es wird zu dem oben festegelegten Punkt "eingabeUhrzeitgoto" gesprungen
+                        } else                                                                                                        //Sollte der eingegebene Wert etwas anderem entsprechen:
+                        {
+                            Console.WriteLine("Fehler, Uhrzeit nicht gefunden!");                                                     //Ausgeben einer Nachricht.
+                            goto eingabeUhrzeitgoto;                                                                                  //Springen zu dem oben genannten Punkt.
+                        }
+                        Console.WriteLine();                                                                                          //Absatz
+
+                        kinoSaaleingabe:                                                                                              //Stichwort für goto. Tritt ein wenn der Saal ungünstig.s
+                        //Hier beginnt die Eingabe zur Asuswahl desK inosaals:
+                        Console.Write("In welchen Kinosal wollen Sie gehen? ");                             //Nachricht zur Auswahl des Saals
+                        eingabeSaal = Convert.ToInt32(Console.ReadLine());                                  //Speichern der Eingabe
+
+                        if(eingabeSaal>anzahlSaele)                                                         //Es wird überprüft, ob die Eingabe in dem möglichen Bereich liegt(kleiner/gleich 5 und größer/gleich 1)
+                        {
+                            Console.WriteLine("Fehler! Der Saal existiert nicht.");
+                            goto kinoSaaleingabe;                                                           //Sollte die eingegebene Zahl nicht in dem Bereich liegen, so wird erneut nach einer Eingabe gefragt.
+                        }else if (eingabeSaal < 1)                                                          //Es wird überprüft, ob die Eingabe in dem möglichen Bereich liegt(kleiner/gleich 5 und größer/gleich 1)
+                        {
+                            Console.WriteLine("Fehler! Der Saal existiert nicht!");                         //Ausgabe, dass der "angeforderte" Saal nicht existiert.
+                            goto kinoSaaleingabe;                                                           //Sollte die eingegebene Zahl nicht in dem Bereich liegen, so wird erneut nach einer Eingabe gefragt.
+                        }
+                        else
+                        {                                                                                   //Hier endet diese Eingabe. Je nach eingabe öffnet sich ein Fenster, zur Auswahl eines Sitzplatzes.
+                            Console.WriteLine("Sie haben erfolgreich Sal {0} ausgewählt!",eingabeSaal);      //Ausgabe, dass die Eingabe erfolgreich war.
+                            csvSaal = eingabeSaal;
+                            Console.Clear();
+
+                            switch (eingabeSaal)                                                            //Switch-Case zur Abfrage der Eingabe.
+                            {
+                                case 1:                                                                     //Sollte Saal 1 ausgewählt worden sein, wird
+                                    seatControllerUI = new SeatControllerUI(sal1, eingabeUhrzeit);          //ein grafisches Fenster erstellt, in der Größe des oben festgelegten Arrays.
+                                    
+                                    break;
+                                case 2:                                                                     //Sollte Saal 2 ausgewählt worden sein, wird
+                                    seatControllerUI = new SeatControllerUI(sal2, eingabeUhrzeit);          //ein grafisches Fenster erstellt, in der Größe des oben festgelegten Arrays.
+                                    
+                                    break;
+                                case 3:                                                                     //Sollte Saal 3 ausgewählt worden sein, wird
+                                    seatControllerUI = new SeatControllerUI(sal3, eingabeUhrzeit);          //ein grafisches Fenster erstellt, in der Größe des oben festgelegten Arrays.
+                                    
+                                    break;
+                                case 4:                                                                     //Sollte Saal 4 ausgewählt worden sein, wird
+                                    seatControllerUI = new SeatControllerUI(sal4, eingabeUhrzeit);          //ein grafisches Fenster erstellt, in der Größe des oben festgelegten Arrays.
+                                   
+                                    break;
+                                case 5:                                                                     //Sollte Saal 5 ausgewählt worden sein, wird
+                                    seatControllerUI = new SeatControllerUI(sal5, eingabeUhrzeit);          //ein grafisches Fenster erstellt, in der Größe des oben festgelegten Arrays.
+                                    
+                                    break;
+                                default:                                                                    //Dies wird ausgeführt, sollte die Ausgabe etwas anderem entsprechen, eigentlich irrelevant, allerdings scheint es nicht zu funktionieren wenn "default" weggelassen wird.
+                                    seatControllerUI = new SeatControllerUI(sal1, "17:00");                 //ein grafisches Fenster erstellt, in der Größe des oben festgelegten Arrays.
+                                    
+                                    break;
+                            }
+                            Application.EnableVisualStyles();                                               //Benötigt zum verwenden von Windows-Forms-Schlüsselwörtern.
+                            Application.Run(seatControllerUI);                                              //Verwenden der Klasse "seatControllerUI".
+                            
+                        }
+
+                    }
+                        
+                    break;                                                      //Ende des ersten "cases".
+                case 'S':                                                       //Sollte der eingegebene Wert "S" entsprechen, wird folgendes gemacht:
+                   
+                    StreamReader sr = new StreamReader(@"D:\test1.txt");        //Erstellen eines "StreamReaders", benötigt um aus einer Textdatei zu lesen.
+                    string fileOutput = sr.ReadToEnd();                         //es wird ein string erstellt, in welchem der alles aus der oben genannten Textdatei gespeichert wird.
+                    MessageBox.Show(fileOutput);                                //Ausgeben des Strings. Verwenden von MessageBox. Dies öffnet ein Fenster welches die Nachricht anzeigt. 
+
+            
+                    break;                  
+
+            }
+            
+
+        }//Main
+    }//class Program
+}//namespace
---
+++ Kino_Pruefungsvorbereitung/SeatControllerUI.cs
@@ -0,0 +1,321 @@
+﻿using CinemaSeatController;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace Kino_Pruefungsvorbereitung
+{
+    public partial class SeatControllerUI : Form
+    {
+        private int[] angaben;              // Private bedeutet das man von dieser Klasse auf die Variable zugreifen kann, würde man jetzt noch einen "getter" dafür schreiben (public int[] getAngaben(){ return [this.]angaben }) könnte man diese auch in anderen klassen benutzten solange man die instance dieser klasse hat. Man könnte das ganze auch einfach ohne "private" machen, wir wollen aber hier doch die richtigen conventions verwenden.
+        private String uhrzeit;             // ^
+        
+
+
+        private int price = 0;
+
+
+        private IList<Sitzplatz> sitzplaetze;                   //Sitzplätze in "Liste" statt Array weil es für mich persönlich, einfacher war das Ziel zu erreichen.
+  
+
+        public SeatControllerUI(int[] angaben, String uhrzeit) //Inhalt des Fensters. (Sitze, Preis etc)
+
+        {
+
+            //Initialisierung, Deklaration und Instanzbildung
+            sitzplaetze = new List<Sitzplatz>();                // init der liste
+
+            int seats = angaben[0] * angaben[1];                // berechnung der gesamten sitze
+            int seatsPerRow = angaben[1];                       // wie viele sitze in einer Reihe sind, bekommt Wert aus "Saal"etc Variable aus "program.cs" class
+            int currentRow = 1;                                 // momentane reihe 
+            price = 0;                                      
+            int currentSeatsInRow = 0;                          // momentane sitze in momentaniger reihe 
+
+            for (int i = 1; i <= seats; i++)                    //for schleide fürs erstellen der Sitze
+            {
+
+                bool premium = false;                           //Bool hat nur "true und false" als Wert. In diesem Fall hat "premium" standartmäßig den Wert "false"                     
+
+                if (currentSeatsInRow == seatsPerRow)           //Wenn "CurrentSeatsInRow" dem Wert von "seatsPerRow" entspricht wird:
+                {
+                    currentRow++;                               //"eine Reihe weiter gegangen"
+                    currentSeatsInRow = 0;                      //und der Wert für "currentSeatsInRow" wird auf 0 gesetzt, fängt also von neu an.
+                }
+
+                if (currentRow <= 5)                            //Falls der Wert von "currenRow" unter 5 ist, also die Reihe, die momentan erstellt wird geringer oder gleich 5 ist, wird:
+                {
+                    premium = true;                             //das Feld als "premium" markiert, sprich, es wird später als "günstiger" angepriesen und in einer anderen Farbe dargestellt.
+                }
+                Sitzplatz seat = new Sitzplatz(premium, currentRow,i);             
+                sitzplaetze.Add(seat);
+                //Console.WriteLine("New Seat created! Premium: " + premium + ", Row: " + seat.getReihe() + ", Number: " + i);      //Nachricht, welche ausgibt, das ein Sitz erstellt wurde.
+                currentSeatsInRow++;
+            }
+
+            this.uhrzeit = uhrzeit;                             //verwenden der privaten Variable "uhrzeit"
+            this.angaben = angaben;                             //verwenden der privaten Variable, sogar Arrays, "angaben"
+            InitializeComponent();                              //Wird automatisch von "windows forms" erstellt.
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; //Fixieren der Größe des grafischen Fensters.
+            this.MaximizeBox = false;                           //^, "MaximizeBox" entscheidet ob das ein Vollbild-Fenster ist.
+            resizeForm();                                       //wird später verwendet um die Größe des Fensters festzulegen.
+            addSitzplaetze();                                   //Hier wird später die Position der Sitzplätze festgelegt
+            setPriceLocation();                                 //Position des Gesamtpreises
+            setPrintLocation();                                 //Position des Knopfes welcher alles in die Textddatei schreibt.
+        }
+
+        public void resizeForm()                                //Hier drin wird die Größe des Fensters bestimmt.
+        {
+            int[] size = new int[2];                            //Array für die Größe des Fensters, 
+            size = calculateFormSize();                         //Das Array "size" bekommt die selben Werte zugewiesen wie das Array "calculateFormSize".
+            this.Width = size[0];                               //Die Breite des Fensters bekommt den Wert des Arrays an Stelle "0", dies entspricht dem ersten Wert des Arrays.
+            this.Height = size[1];                              //Die Höhe des Fensters bekommt den Wert des Arrays an Stelle "1", dies entspricht dem zweiten Wert des Arrays.
+            this.Update();                                      //Update/Erneuern des ganzen.
+        }
+
+        public void setPrintLocation()                          //
+        {
+            int x = this.Width;
+            int y = this.Height;
+            y -= 100;
+            x -= 125;
+
+            printButton.Location = new Point(x, y);
+
+            printButton.Click += new EventHandler(printButtonClickEvent);
+            
+        }
+
+        protected void printButtonClickEvent(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(@"D:\test1.txt");
+
+
+            
+
+            // Man könnte den folgenden Part auch ohne Arrays machen, wird aber aus Lernzwecken mit Arrays gemacht.
+
+            int b = 0;
+            for(int i = 0; i < sitzplaetze.Count(); i++) {
+
+                Sitzplatz sitzplatz = sitzplaetze.ElementAt(i);
+
+                if (!sitzplatz.istVerfuegbar())
+                    b++;       
+            }
+
+            if(b == 0)
+            {
+                MessageBox.Show("Du hast keine Plätze ausgewählt!"); // "Errormessage" wenn keine Sitzplätze ausgewählt wurden
+                return;
+            }
+
+            Sitzplatz[] sitzplatzArray = new Sitzplatz[b];
+
+            b = 0;
+            for(int i = 0; i< sitzplaetze.Count(); i++)
+            {
+                Sitzplatz sitzplatz = sitzplaetze.ElementAt(i);
+                if (!sitzplatz.istVerfuegbar())
+                {
+
+                    sitzplatzArray[b] = sitzplatz;
+                    b++;
+                }
+            }
+
+
+            // Hier wird ein StringBuilder benutzt um die Plätze in einem String aufzulisten.
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            bool c = false;
+
+            foreach(Sitzplatz sitzplatz in sitzplatzArray)
+            {
+                if(c == false) {
+                stringBuilder.Append(sitzplatz.getNumber());
+                c = true;
+                }
+                else
+                {
+                    stringBuilder.Append(",").Append(" " + sitzplatz.getNumber());
+                }
+            }
+
+            Program.csvPlatz = stringBuilder.ToString();
+
+            string output=("Uhrzeit: "+ Program.csvUhrzeit);
+            string output1 = ("Saal: " + Program.csvSaal);
+            string output2 = ("Plätze: " + Program.csvPlatz);
+            string output3 = ("Kosten: " + price + " Euro.");
+            sw.WriteLine("Reservierung!");
+            sw.WriteLine(output);
+            sw.WriteLine("{0}.         {1}.",output1,output2);
+            sw.WriteLine(output3);
+            sw.Close();
+
+
+        }
+
+        public void setPriceLocation()
+        {
+            int x = this.Width;
+            int y = this.Height;
+            y -= 100;
+            x -= (this.Width - 70);
+
+          
+            premiumPriceLabel.Text = "Premium Preis: " + Sitzplatz.normalPrice + "€";
+            priceLabel.Text = "Preis: " + Sitzplatz.premiumPrice + "€";
+
+            costLabel.Size =new Size(100,40);
+            costLabel.Location = new Point(x, y);
+            costLabel.Name = "Preis";
+            costLabel.Font = new Font("Calibri", 20, FontStyle.Underline);
+            costLabel.Text = "Gesamtpreis: 0 €";
+            costLabel.Update();
+            this.Update();
+        }
+
+        public void changePriceLabel()
+        { 
+            costLabel.Text = "Gesamtpreis: " + price + " €";
+            costLabel.Update();
+            this.Update();
+        }
+
+        public void addSitzplaetze()
+        {
+
+
+            int x = 20;             //"Startposition" der Knöpfe/Buttons, veränderbar
+            int y = 50;             //^
+
+
+            int buttonsInRow = 0;   // Wie viele Knöpfe momentan in der momentanen Reihe sind
+
+
+
+            for (int i = 0; i < sitzplaetze.Count; i++)
+            {
+                Sitzplatz sitzplatz = sitzplaetze.ElementAt(i);
+
+
+                int number = i + 1;
+
+
+                Button button = new Button();   // erstellen des "Knopfes"
+                button.Width = 35;              // Größes des Knopfes
+                button.Height = 35;             // ^
+                this.Controls.Add(button);      // Knopf zur Form hinzufügen
+                button.Text = number + "";      // Text des Buttons ändern.
+                button.Location = new Point(x, y); // Button auf die richtige Position in der Form setzten.
+
+                button.Click += new EventHandler(seatButtonClickEvent); // Event hinzufügen das beim Klicken auf dem Button ausgeführt wird.
+                button.BackColor = sitzplatz.istVerfuegbar() ? (sitzplatz.istPremium() ? Color.DarkGreen : Color.Green) : Color.Red; // Button farbe
+                button.Show(); // anzeigen des buttons
+
+                buttonsInRow++; 
+
+
+                if (angaben[1] / 2 == buttonsInRow)
+                {
+                    x += 95;
+                }
+                else if (angaben[1] == buttonsInRow)
+                {
+                    x = 20;
+                    y += 45;
+                    buttonsInRow = 0;
+                }
+                else
+                {
+                    x += 45;
+                }
+            }
+            Console.WriteLine("Added seats!");
+            this.Update();
+        }
+
+        protected void seatButtonClickEvent(object sender, EventArgs e)
+        {
+            //Instanzbildung
+            int[] sitzNummer = new int[sitzplaetze.Count];
+
+            Button button = sender as Button;
+            
+            Console.WriteLine("Button clicked -> " + button.Text);
+
+            Sitzplatz seat = sitzplaetze.ElementAt((int.Parse(button.Text) - 1));
+           
+       
+            
+                
+            
+           
+            seat.setVerfuegbar(!seat.istVerfuegbar());
+            button.BackColor = seat.istVerfuegbar() ? (seat.istPremium() ? Color.DarkGreen : Color.Green) : Color.DimGray; // IF else in einer Zeile. man könnte auch if(abfrage){ dann }else{sonst} benutzten
+
+
+            if (!seat.istVerfuegbar()) // Ausrufezeichen ist if(boolean == false) ohne Ausrufenzeichen wäre es if(boolean == true)
+            { 
+
+                price += seat.istPremium() ? Sitzplatz.premiumPrice : Sitzplatz.normalPrice; // IF else in einer Zeile. man könnte auch if(abfrage){ dann }else{sonst} benutzten - Wenn der Sitz kein "Premiumsitzt" ist wird der normale Preis gezahlt, wenn doch wird der Premium Preis bezahlt
+
+            }
+            else
+            {
+                price -= seat.istPremium() ? Sitzplatz.premiumPrice : Sitzplatz.normalPrice; // IF else in einer Zeile. man könnte auch if(abfrage){ dann }else{sonst} benutzten
+            }
+
+            if(price < 0)
+            {
+             //   price = 0;
+            }
+            changePriceLabel();
+
+            // set price
+
+            this.Update();
+
+        }
+
+        private int[] calculateFormSize()
+        {
+            int[] size = new int[2];
+
+
+            int seatWidt = 35; //   seat widt
+            int seatGap = 10;  // seat gap
+            int seatHeight = 35; // seat height
+
+
+
+            // Form size
+            // we´re starting with the widt
+
+            int corridorWidt = 50; // size of the corridor widt between the seats
+
+
+            int widt = 40 /* size @ edges */ + (seatWidt * angaben[1]) + (seatGap * angaben[1]) + corridorWidt + 20;
+            int height = 100 /* size @ edges */ + (seatHeight * angaben[0]) + (seatGap * angaben[0]) + 90;
+
+            Console.WriteLine("widt: " + widt);
+            Console.WriteLine("height: " + height);
+
+            size[0] = widt;
+            size[1] = height;
+            return size;
+        }
+
+    }
+}
